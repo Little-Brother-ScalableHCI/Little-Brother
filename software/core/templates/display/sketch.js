6054c6b9d9a8f5c8afc282215ecd49d75abca399
Louis BADR <louis.badr@edu.devinci.fr>
2025-01-12 14:32:50 +0800

    Demo ready (?)

--- software/core/templates/display/sketch.js
+++ software/core/templates/display/sketch.js
@@ -5,6 +5,9 @@ var itemFound;
 var spotlight;
 var arrow;
 
+var orange;
+var blue;
+
 // Simple spotlight effect
 class SearchingItem {
   constructor(diameter) {
@@ -20,7 +23,7 @@ class SearchingItem {
 
 // Makes a circle grow from minDiameter to maxDiameter, while decreasing strokeWeight from maxStrokeWeight to 0
 class GrowingCircle {
-  constructor(x, y, minDiameter, maxDiameter, maxStrokeWeight, speed = 2, state = 0) {
+  constructor(x, y, minDiameter, maxDiameter, maxStrokeWeight, speed = 4, state = 0) {
     this.x = x;
     this.y = y;
     this.minDiameter = minDiameter;
@@ -30,12 +33,13 @@ class GrowingCircle {
     this.strokeWeight = maxStrokeWeight;
     this.speed = speed;
     this.state = state;
+    this.blur = 3;
   }
 
   display() {
-    drawingContext.filter = 'blur(6px)';
+    drawingContext.filter = `blur(${this.blur}px)`;
     noFill();
-    stroke(255);
+    stroke(orange);
     strokeWeight(this.strokeWeight);
     circle(this.x, this.y, this.diameter);
     // reset the filter
@@ -46,9 +50,11 @@ class GrowingCircle {
     if (this.diameter >= this.maxDiameter) {
       this.diameter = this.minDiameter;
       this.strokeWeight = this.maxStrokeWeight;
+      this.blur = 3;
     }
     this.strokeWeight = this.maxStrokeWeight * (1 - (this.diameter - this.minDiameter) / (this.maxDiameter - this.minDiameter));
     this.diameter += this.speed;
+    this.blur += 0.15;
   }
 }
 
@@ -85,41 +91,78 @@ class ItemFound {
   }
 }
 
-class FollowMe {
-  constructor(angle) {
-    this.angle = angle
+class MovingArrow {
+  constructor(angle, speed = 22) {
+    this.angle = angle;
+    this.speed = speed;
+    this.x = w / 2;
+    this.y = h / 2;
+    this.size = h / 4;
+    this.maxBlur = 40; // Maximum blur at the edges
+    this.minBlur = 0; // Minimum blur at the center
+    this.nextAngle = null;
   }
 
   display() {
+    // Calculate blur based on distance from the center
+    let distFromCenter = dist(this.x, this.y, w/2, h/2);
+    let maxDist = dist(0, 0, w/2, h/2);
+    // Map distance to blur value
+    this.blur = map(distFromCenter, 0, maxDist, this.minBlur, this.maxBlur);
+    drawingContext.filter = `blur(${this.blur}px)`;
     stroke(255);
     strokeWeight(50);
-    translate(w/2, h/2);
+    translate(this.x, this.y);
     rotate(45 + this.angle);
-    line(0, 0, 0, h/4);
-    line(0, 0, h/4, 0);
+    line(0, 0, 0, this.size);
+    line(0, 0, this.size, 0);
+    // reset filter
+    drawingContext.filter = 'none';
+  }
+
+  animate() {
+    // Wrap around the screen - reorient if nextAngle is set
+    if (this.x - this.size > w || this.x + this.size < 0 || this.y - this.size > h || this.y + this.size < 0) {
+      this.x = w - this.x;
+      this.y = h - this.y;
+      if (this.nextAngle) {
+        this.x = w / 2;
+        this.y = h / 2;
+        this.angle = this.nextAngle;
+        this.nextAngle = null;
+      }
+    }
+    // Increment the position of the arrow
+    this.x += this.speed * sin(this.angle);
+    this.y -= this.speed * cos(this.angle);
+    
   }
 
   update(angle) {
-    this.angle = angle;
+    this.nextAngle = angle;
   }
 }
 
+
 function setup() {
   createCanvas(w, h);
-  frameRate(60);
+  frameRate(30);
   angleMode(DEGREES);
-  // spotlight = new SearchingItem(h * 0.95);
+  orange = color("#fca103");
+  blue = color("#2e45ed");
+
+  spotlight = new SearchingItem(h * 0.95);
   itemFound = new ItemFound(w/2, h/2, h/3, h/1.5, 10);
-  // arrow = new FollowMe(0);
+  arrow = new MovingArrow(0);
 }
 
 function draw() {
   background(0);
-  itemFound.display();
-  itemFound.animate();
+  // itemFound.display();
+  // itemFound.animate();
   // spotlight.display();
-  // arrow.display();
-  // arrow.update(frameCount % 360);
+  arrow.display()
+  arrow.animate();
 }
 
 window.onresize = function() {
